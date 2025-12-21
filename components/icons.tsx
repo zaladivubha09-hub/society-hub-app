b5155ea24891572b79c8d11e8b7c49de38c6d2f0
zaladivubha09-hub <zaladivubha09@gmail.com>
2025-12-22 00:26:27 +0530

    Add files via upload

--- components/icons.tsx
+++ components/icons.tsx
@@ -140,7 +140,6 @@ export const CreditCardIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) =
   </Icon>
 );
 
-// New Icons for Login Page
 export const UserIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
   <Icon {...props}>
     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
